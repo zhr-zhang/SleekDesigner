a0bc4124f06b68ce66dd1238639e46b2c0bede9f
Haoran Zhang <zhr.zhang@outlook.com>
2023-11-11 16:35:53 +0800

    Update figure.py

--- scripts/figure.py
+++ scripts/figure.py
@@ -66,6 +66,13 @@ class Arc(Figure):
         n = (xp * self.y1 - self.x1 * yp) / (self.x2 * self.y1 - self.x1 * self.y2)
         if self.direction == 0:
             print("an arc can't have an angle of 180 or 0")
+
+            # TODO complete this part
+            """为了判断点 P 是否在向量 A 的左侧或右侧，我们可以使用向量的内积（点乘）来计算。给定起点 O，向量 A 和点 P 的坐标分别为 (x1, y1), (x2, y2) 和 (x3, y3)。
+            首先，我们计算向量 A 和向量 OP（从 O 指向 P）的内积：
+            A·OP = (x1 - x2) * (x3 - x1) + (y1 - y2) * (y3 - y1)
+            如果 A·OP 的结果为负数，那么向量 A 和向量 OP 之间的夹角在第二象限或第三象限，也就是说点 P 在向量 A 的左侧。如果 A·OP 的结果为正数，那么向量 A 和向量 OP 之间的夹角在第一象限或第四象限，也就是说点 P 在向量 A 的右侧。如果 A·OP 的结果为零，那么点 P 与向量 A 的起点 O 重合。
+            需要注意的是，这种方法只适用于二维平面上的向量和点。对于更高维度的向量和点，需要相应地增加计算维度。"""
             distance2 = 0
         elif self.direction > 0:
             if m >= 0 and n >= 0:
